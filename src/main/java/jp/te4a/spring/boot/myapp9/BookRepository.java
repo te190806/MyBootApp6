734ffa7ae5d1e7a077fd8ca41e3cbbaf72964773
te190806 <190806@jc-21.jp>
2022-06-15 11:07:43 +0900

    プロジェクトkadai9のBookRepository.javaのimport部分（List）を削除した。※使用しないと考えたため。

--- src/main/java/jp/te4a/spring/boot/myapp9/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookRepository.java
@@ -1,9 +1,6 @@
 package jp.te4a.spring.boot.myapp9;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
